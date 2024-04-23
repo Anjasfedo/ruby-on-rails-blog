0291ddc1b05e6415fce291418ff9d6633215bb25
AnjasFedo <fedoaf98@gmail.com>
2024-04-23 22:47:51 +0700

    Concern Visible

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,14 @@ class CommentsController < ApplicationController
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
         redirect_to article_path(@article)
-      end
+    end
+
+    def destroy
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+        @comment.destroy
+        redirect_to article_path(@article), status: :see_other
+    end
     
     private
         def comment_params
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :article
+  include Visible
   
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
+  belongs_to :article
 end
