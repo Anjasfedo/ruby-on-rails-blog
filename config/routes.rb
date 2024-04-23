cbb966896027092d58c0a69c7fc59d65571e8c8d
AnjasFedo <fedoaf98@gmail.com>
2024-04-23 21:57:20 +0700

    Routing with Resource

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  resources :articles
 end
