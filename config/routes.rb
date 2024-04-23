7bb43dc9a1d5185cfeba684aec11d3db2afff6a6
AnjasFedo <fedoaf98@gmail.com>
2024-04-23 22:35:42 +0700

    Comment Field

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
 
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
